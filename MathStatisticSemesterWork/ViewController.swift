537800471b4531f0fe29074cad3f868106511613
Murtazin <renatmurtazin@yahoo.com>
2022-04-17 15:29:44 +0300

    Initial Commit

---
+++ MathStatisticSemesterWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MathStatisticSemesterWork
+//
+//  Created by Renat Murtazin on 17.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
